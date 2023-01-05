0d80fd40de07312b1cb1d6657c2ff41a0bf9ba18
Meruszka <merski70@gmail.com>
2023-01-05 21:52:03 +0100

    reformat code

--- RandomWord/src/types/word/wordResolver.ts
+++ RandomWord/src/types/word/wordResolver.ts
@@ -2,9 +2,11 @@
 // Should be replaced with database OR file storage...
 import fs from 'fs';
 
-
 const getRandomWord = (args: { language: string }): string | undefined => {
-    const lines = fs.readFileSync(`${process.cwd()}/src/types/word/data/${args.language}.txt`, 'utf8');
+    const lines = fs.readFileSync(
+        `${process.cwd()}/src/types/word/data/${args.language}.txt`,
+        'utf8'
+    );
     const words = lines.split('\n');
     let word = words[Math.floor(Math.random() * words.length)];
     word = word.replace('\r', '');
@@ -12,70 +14,89 @@ const getRandomWord = (args: { language: string }): string | undefined => {
 };
 
 const createWord = (args: { input: { word: string; language: string } }): string => {
-    fs.writeFile(`${process.cwd()}/src/types/word/data/${args.input.language}.txt`, args.input.word, (err) => {
-        if (err) {
-            console.error(err)
-            return
+    fs.writeFile(
+        `${process.cwd()}/src/types/word/data/${args.input.language}.txt`,
+        args.input.word,
+        (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
         }
-    });
+    );
     return args.input.word;
 };
 
 const deleteWord = (args: { input: { word: string; language: string } }): string => {
-    fs.readFile(`${process.cwd()}/src/types/word/data/${args.input.language}.txt`, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        const lines = data.split('\n');
-        const index = lines.indexOf(args.input.word);
-        if (index > -1) {
-            lines.splice(index, 1);
-        }
-        fs.writeFile(`${process.cwd()}/src/types/word/data/${args.input.language}.txt`, lines.join('\n'), (err) => {
+    fs.readFile(
+        `${process.cwd()}/src/types/word/data/${args.input.language}.txt`,
+        'utf8',
+        (err, data) => {
             if (err) {
-                console.error(err)
-                return
+                console.error(err);
+                return;
             }
-        });
-    });
+            const lines = data.split('\n');
+            const index = lines.indexOf(args.input.word);
+            if (index > -1) {
+                lines.splice(index, 1);
+            }
+            fs.writeFile(
+                `${process.cwd()}/src/types/word/data/${args.input.language}.txt`,
+                lines.join('\n'),
+                (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                }
+            );
+        }
+    );
     return args.input.word;
 };
 
 const updateWord = (args: { input: { word: string; language: string } }): string => {
-    fs.readFile(`${process.cwd()}/src/types/word/data/${args.input.language}.txt`, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        const lines = data.split('\n');
-        const index = lines.indexOf(args.input.word);
-        if (index > -1) {
-            lines[index] = args.input.word;
-        }
-        fs.writeFile(`${process.cwd()}/src/types/word/data/${args.input.language}.txt`, lines.join('\n'), (err) => {
+    fs.readFile(
+        `${process.cwd()}/src/types/word/data/${args.input.language}.txt`,
+        'utf8',
+        (err, data) => {
             if (err) {
-                console.error(err)
-                return
+                console.error(err);
+                return;
             }
-        });
-    });
+            const lines = data.split('\n');
+            const index = lines.indexOf(args.input.word);
+            if (index > -1) {
+                lines[index] = args.input.word;
+            }
+            fs.writeFile(
+                `${process.cwd()}/src/types/word/data/${args.input.language}.txt`,
+                lines.join('\n'),
+                (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                }
+            );
+        }
+    );
     return args.input.word;
 };
 
 const resolvers = {
     Query: {
-        randomWord: (_:any, args: { language: string }) => getRandomWord(args),
+        randomWord: (_: any, args: { language: string }) => getRandomWord(args),
     },
     Mutation: {
         createWord: (args: { input: { word: string; language: string } }) =>
-        createWord(args),
+            createWord(args),
         deleteWord: (args: { input: { word: string; language: string } }) =>
-        deleteWord(args),
+            deleteWord(args),
         updateWord: (args: { input: { word: string; language: string } }) =>
-        updateWord(args),
+            updateWord(args),
     },
-
 };
 
 export default resolvers;
